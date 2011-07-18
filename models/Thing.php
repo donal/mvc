97af1377ece835e058931c74a15a256303fa6d3a
Donal Ellis <donalellis@gmail.com>
2011-07-18 22:41:59 +1000

    updated Thing model to use mysql on yallara

--- models/Thing.php
+++ models/Thing.php
@@ -7,7 +7,7 @@ class Thing {
   private static $database_pw    = 'mvc';
 
   public static function retrieve(array $data = array()) {
-    $dbc = mysql_connect('localhost', self::$database_user, self::$database_pw);
+    $dbc = mysql_connect('yallara.cs.rmit.edu.au:50000', self::$database_user, self::$database_pw);
     mysql_select_db(self::$database_name);
 
     $sql = 'SELECT * FROM things';
@@ -22,9 +22,9 @@ class Thing {
         }
       }
 
-      $result = mysql_query($sql);
+      $result = @mysql_query($sql);
       $rows = array();
-      while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+      while ($row = @mysql_fetch_array($result, MYSQL_ASSOC)) {
         $rows[] = $row;
       }
 
