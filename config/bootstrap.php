7a9f0f8cf57d861d43ec5c9709a024f9f6a5e0a2
Donal Ellis <donal.ellis@rmit.edu.au>
2011-08-06 10:33:50 +1000

    added new routes to bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,11 +1,7 @@
 <?php
 
-// require_once (APP_PATH . DS . 'config' . DS . 'config.php');
-// require_once (APP_PATH . DS . 'libraries' . DS . 'config.php');
-
 define('LIBRARY_PATH', APP_PATH . DS . 'libraries');
 
-// TODO
 // 1. sanitise (remove magic quotes, slashes, global vars)
 // 2. do the routing - convert path into controller and action
 // 3. autoload objects
@@ -20,6 +16,8 @@ $routes['#^/things$#i'] = array('controller' => 'things', 'action' => 'index');
 $routes['#^/things/new$#i'] = array('controller' => 'things', 'action' => 'add');
 $routes['#^things/create$#i'] = array('controller' => 'things', 'action' => 'create');
 $routes['#^/things/([0-9]{1,5})$#i'] = array('controller' => 'things', 'action' => 'show');
+$routes['#^/things/([0-9]{1,5})/edit$#i'] = array('controller' => 'things', 'action' => 'edit');
+$routes['#^/things/([0-9]{1,5})/update$#i'] = array('controller' => 'things', 'action' => 'edit');
 
 // database connection
 
